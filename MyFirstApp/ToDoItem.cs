ae4be8694a9ea044aed1d1baf6be1844c7f7ce7f
AFrownyFace <bkayala22@gmail.com>
2022-06-02 16:34:41 -0400

    Add project files.

---
+++ MyFirstApp/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+
+
+    // create a todoItem class
+    // and have the fields Title and IsDone on it 
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
